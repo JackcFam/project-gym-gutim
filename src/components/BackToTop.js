baedf60f51e6840c26661161764fca7ce4322437
NhatPham <43815615+JackcFam@users.noreply.github.com>
2020-10-24 00:21:06 +0700

    up

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -9,13 +9,13 @@ import ScrollUpButton from "react-scroll-up-button";
 function BackToTop(props) {
     return (
       <ScrollUpButton
-        ShowAtPosition={150}
+        ShowAtPosition={850}
         ContainerClassName="back-to-top"
         TransitionClassName="back-to-top-trasition "
       >
         <div>
           <span className="btn-back-to-top">
-            <i className="fas fa-arrow-up"></i>
+            <i className="fas fa-long-arrow-alt-up"></i>
           </span>
         </div>
       </ScrollUpButton>
